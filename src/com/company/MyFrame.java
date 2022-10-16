084f880a82eece758bf6739e1fea74531912872c
comptergeeks <F@rhan0108>
2022-10-16 01:04:50 -0700

    Redid code due to requirements. Instead am now focusing on the implmentation of new tabs using Jpanel's so I can control the z-orientation, of each new tab.

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -1,33 +1,38 @@
 package com.company;
+import org.w3c.dom.css.RGBColor;
+
 import javax.swing.*;
-import javax.swing.event.InternalFrameEvent;
-import javax.swing.event.InternalFrameListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 public class MyFrame extends JPanel implements ActionListener {
     public JFrame frame = new JFrame("Windows XP");
     public JPanel panel = new JPanel();
+    int x = 20;
     public JLabel background = new JLabel(new ImageIcon("/Users/farhankhan/IdeaProjects/WindowsVista/src/com/company/assets/windowsbackground.jpeg"));
     ImageIcon scaledInternet = new ImageIcon(new ImageIcon("/Users/farhankhan/IdeaProjects/WindowsVista/src/com/company/assets/internet.png").getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT));
     ImageIcon scaledFileExp = new ImageIcon(new ImageIcon("/Users/farhankhan/IdeaProjects/WindowsVista/src/com/company/assets/fileexplorer.png").getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT));
     public JButton internetExp = new JButton(scaledInternet);
     public JButton fileExp = new JButton(scaledFileExp);
-    ArrayList<HashMap> listOfTabs = new ArrayList<HashMap>();
+    ArrayList<JPanel> arrPan = new ArrayList<>();
 
     public void setFrame() {
         frame.setLayout(new BorderLayout());
+        frame.add(background);
         frame.setSize(1600, 900);
         frame.setResizable(true);
-        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(background);
         frame.setVisible(true);
-
     }
+
     public void setPanel() {
         frame.add(panel, BorderLayout.EAST);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
@@ -43,77 +48,31 @@ public class MyFrame extends JPanel implements ActionListener {
         fileExp.setBorder(null);
         //button array forloop?
     }
-    public void createNewWindow(String name) {
-        JInternalFrame jInternalFrame = new JInternalFrame();
-        InternalFrameListener e = new InternalFrameListener() {
-            @Override
-            public void internalFrameOpened(InternalFrameEvent e) {
-            }
 
-            @Override
-            public void internalFrameClosing(InternalFrameEvent e) {
-
-            }
-
-            @Override
-            public void internalFrameClosed(InternalFrameEvent e) {
-                //need to get title of array to make sure correct one is drawing.
-                //array of maps?
-                String titleToRemove = jInternalFrame.getTitle();
-                for (int i = 0; i < listOfTabs.size(); i++) {
-                    if (listOfTabs.get(i).containsValue(titleToRemove)) {
-                        listOfTabs.remove(i);
-                    }
-                }
-            }
-
-            @Override
-            public void internalFrameIconified(InternalFrameEvent e) {
-
-            }
-
-            @Override
-            public void internalFrameDeiconified(InternalFrameEvent e) {
-
-            }
-
-            @Override
-            public void internalFrameActivated(InternalFrameEvent e) {
-
-
-            }
-
-            @Override
-            public void internalFrameDeactivated(InternalFrameEvent e) {
-
-            }
-        };
-        jInternalFrame.setLocation(450, 300);
-        jInternalFrame.setSize(500, 300);
-        jInternalFrame.setTitle(name);
-        jInternalFrame.setClosable(true);
-        jInternalFrame.setResizable(true);
-        jInternalFrame.addInternalFrameListener(e);
-        jInternalFrame.setVisible(true);
-        HashMap mMap = new HashMap();
-        mMap.put("title",jInternalFrame.getTitle());
-        mMap.put("frame", jInternalFrame);
-        listOfTabs.add(mMap);
-        for (int i = 0; i < listOfTabs.size(); i++) {
-            frame.add(jInternalFrame);
-        }
-        frame.repaint();
+    public void createNewWindow() {
+        Random rand = new Random();
+        JPanel panel2 = new JPanel();
+        panel2.setSize(500, 500);
+        panel2.setBackground(new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
+        panel2.setLocation(rand.nextInt(255), rand.nextInt(255));
+        drawerFromArray();
+        arrPan.add(panel2);
+        repaint();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
         if (e.getSource() == internetExp) {
-            String name = "Internet Explorer" + listOfTabs.size();
-            createNewWindow(name);
+            createNewWindow();
             //make if pressed, and then reorder array based on that
         }
     }
-
+    public void drawerFromArray() {
+            for (int i = 0; i < arrPan.size(); i++) {
+                frame.add(arrPan.get(i));
+                frame.pack();
+                repaint();
+            }
+    }
 }
 
