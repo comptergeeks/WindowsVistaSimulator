5181bd438fecda58d9f4c8a9257dc4ecb56d4a8c
comptergeeks <F@rhan0108>
2022-10-15 19:05:09 -0700

    Added in panel and have design working.

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -1,32 +1,43 @@
 package com.company;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MyFrame {
+public class MyFrame extends JPanel implements ActionListener {
     public JFrame frame = new JFrame("Windows XP");
     public JPanel panel = new JPanel();
     public JLabel background = new JLabel(new ImageIcon("/Users/farhankhan/IdeaProjects/WindowsVista/src/com/company/assets/windowsbackground.jpeg"));
-    public JButton internetexp = new JButton(new ImageIcon("/Users/farhankhan/IdeaProjects/WindowsVista/src/com/company/assets/internet.png"));
+    ImageIcon scaledInternet = new ImageIcon(new ImageIcon("/Users/farhankhan/IdeaProjects/WindowsVista/src/com/company/assets/internet.png").getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT));
+
+    public JButton internetexp = new JButton(scaledInternet);
     public void setFrame() {
-        frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
         frame.setSize(1600, 900);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(background);
+        frame.setVisible(true);
 
     }
     public void setPanel() {
-        frame.add(panel);
-        panel.setSize(1600, 900);
-        panel.setVisible(true);
-        panel.add(background);
+        frame.add(panel, BorderLayout.EAST);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setSize(100, 900);
         panel.add(internetexp);
-        internetexp.setVisible(true);
+        panel.setOpaque(false);
+        internetexp.addActionListener(this);
         internetexp.setBackground(null);
         internetexp.setBorder(null);
+        internetexp.setSize(200, 200);
+        internetexp.setVisible(true);
         System.out.println("successful");
-        panel.repaint();
-        //frame.add(background);
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
 
